a0a074a56d2f5d0a28aae3282bfb4b988d706fdd
Noah Baxley <noah.baxley@gmail.com>
2025-02-05 01:01:52 -0600

    Add rule where front must see back

---
+++ ts/Exploration/Rules/frontCanSeeSnakeBack.ts
@@ -0,0 +1,64 @@
+import accessNodeRelation from "../../Board/accessNodeRelation.js";
+import getSnakeBoardNodes from "../../Snake/getSnakeBoardNodes.js";
+import { BoardNode, DIRECTION } from "../../snakeNodes.js";
+import Expedition from "../expedition.js";
+
+/**
+ * In order that a snake could kind-of represent a Hamiltonian cycle, we need
+ * the head to physically be able to access where the next segment will be
+ * added. What this really does is allow the snake to circle back around if it
+ * needs to
+ */
+export default function frontCanSeeSnakeBack(e: Expedition): boolean {
+
+	// A list of already explored nodes
+	// Populate it with the snake nodes
+	const explored: BoardNode[] = getSnakeBoardNodes(e.snake);
+	
+	// Explore from the front...
+	const startNode: BoardNode = e.snake.snakeFront.boardSpaceNode;
+	// Try to find the back...
+	const endNode: BoardNode = e.snake.snakeBack.boardSpaceNode;
+	console.log(`(${startNode.board_x}, ${startNode.board_y}) - (${endNode.board_x}, ${endNode.board_y})`);
+
+	// List of nodes to explore
+	const toExplore: BoardNode[] = [startNode];
+
+	let foundEndNode = false;
+
+	while (toExplore.length > 0 && !foundEndNode) {
+
+		const n = toExplore.pop();
+
+		// If we can see the endNode, exit
+		// if (n == endNode) {
+		// 	foundEndNode = true;
+		// 	break;
+		// }
+
+		const expansions: BoardNode[] = [
+			accessNodeRelation(n, DIRECTION.north),
+			accessNodeRelation(n, DIRECTION.east),
+			accessNodeRelation(n, DIRECTION.south),
+			accessNodeRelation(n, DIRECTION.west),
+		];
+
+		for (const p of expansions) {
+
+			if (p == endNode) {
+				foundEndNode = true;
+				break;
+			}
+
+			if (explored.includes(p))
+				continue;
+			else {
+				explored.push(p);
+				toExplore.push(p);
+			}
+		}
+		
+	}
+
+	return foundEndNode;
+}
