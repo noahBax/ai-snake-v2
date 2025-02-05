7d9d6c8fd0875b64e10433eee55ac45126b5f185
Noah Baxley <noah.baxley@gmail.com>
2025-02-05 00:57:39 -0600

    Enhance console

--- ts/consoleInterface.d.ts
+++ ts/consoleInterface.d.ts
@@ -1,4 +1,5 @@
 import Apple from "./Board/apple.js";
+import { CompassNode } from "./Exploration/duplicateFinder.js";
 import { BoardNode, DIRECTION, SnakeSummary } from "./snakeNodes.js";
 
 export {};
@@ -9,6 +10,9 @@ declare global {
 		snakeSummary: SnakeSummary;
 		emptyNode: BoardNode;
 		keysBuffer: DIRECTION[];
-		snakeDrawBuffer: [SnakeSummary, Apple, number][]; 
+		snakeDrawBuffer: [SnakeSummary, Apple, number, DIRECTION[]?][]; 
+		frameBufferIndex: number;
+		duplicateBoard: CompassNode[];
+		expandAtNode;
 	}
 }
