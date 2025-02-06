0c44aba477e18e9e4aed646f3d55c6a26d088d8e
Noah Baxley <noah.baxley@gmail.com>
2025-02-06 13:03:16 -0600

    Add children and debt forgiveness to lineages

--- ts/Exploration/LineageManager/lineage.ts
+++ ts/Exploration/LineageManager/lineage.ts
@@ -1,22 +1,42 @@
-class FamilyNode {
+export class FamilyNode {
 	parent: FamilyNode | false;
+	children: FamilyNode[];
 	familyDebt: number;
 	cumulativeDebt: number;
 
 	constructor(parent: FamilyNode | false) {
 		this.parent = parent;
 		this.familyDebt = 0;
-		this.calculateCumulativeDebt();
+		this.cumulativeDebt = 0;
+		this.children = [];
 	}
 
-	calculateCumulativeDebt(): void {
+	calculateCumulativeDebt(interest: number): void {
 
 		this.cumulativeDebt = 0;
 		let node: FamilyNode = this;
+		let layers = 0;
 		while (true) {
+			
+			this.cumulativeDebt += node.familyDebt * interest**layers;
+			
+			if (!node.parent)
+				break;
+			else
+				node = node.parent;
 
-			this.cumulativeDebt += node.familyDebt;
+			layers++;
+		}
+	}
 
+	incrementDebt(amt=1, discount=1.0, tax=0.0): void {
+		
+		let node: FamilyNode = this;
+		let inc = amt;
+		while (true) {
+			node.familyDebt += inc;
+			inc = inc * discount;
+			inc += tax * node.familyDebt;
 			if (!node.parent)
 				break;
 			else
@@ -24,15 +44,9 @@ class FamilyNode {
 		}
 	}
 
-	incrementDebt(): void {
-		
-		let node: FamilyNode = this;
-		while (true) {
-			node.familyDebt++;
-			if (!node.parent)
-				break;
-			else
-				node = node.parent;
+	forgiveDebt(amt=0.01): void {
+		for (const c of this.children) {
+			c.forgiveDebt(amt);
 		}
 	}
 }
