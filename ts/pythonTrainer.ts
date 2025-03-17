aac67372502b383b5c3388d56ce48399253d086a
Noah Baxley <noah.baxley@gmail.com>
2025-03-17 17:51:46 -0500

    Pass config as JSON string

--- ts/pythonTrainer.ts
+++ ts/pythonTrainer.ts
@@ -7,53 +7,8 @@ import { AI_SCORE, tallyScore } from "./aiScoring.js";
 
 function main(): void {
 
-	// Parameters for the config are passed as a string of numbers separated by
-	// spaces. They come in a specific order corresponding to specific keys of
-	// the config.
-	const a = argv[2].split(' ')
-	const objKeys: (keyof Configuration)[] = [
-		"avoidanceVisitPenalty",
-		"avoidanceCoolDown",
-		"snakePathDecrement",
-		"snakePathFalloff",
-		"utilityCurvyLengthBack",
-		"utilityCurvyStraightLength",
-		"utilityCurvyTurnPenaltyDefault",
-		"utilityStraightStraightLength",
-		"utilityStraightTurnBonusDivisor",
-		"utilityStraightLengthBack",
-		"sizeOneGroups",
-		"utilityCurvy",
-		"largestGroupSize",
-		"utilityStraight",
-		"numGroups",
-		"canSeeTail",
-		"appleTaxiMultiplier",
-	]
-	const config: Configuration = {
-		avoidanceVisitPenalty: 0,
-		avoidanceCoolDown: 0,
-		snakePathDecrement: 0,
-		snakePathFalloff: 0,
-		utilityCurvy: 0,
-		utilityCurvyLengthBack: 0,
-		utilityCurvyStraightLength: 0,
-		utilityCurvyTurnPenaltyDefault: 0,
-		utilityStraight: 0,
-		utilityStraightStraightLength: 0,
-		utilityStraightTurnBonusDivisor: 0,
-		utilityStraightLengthBack: 0,
-		sizeOneGroups: 0,
-		largestGroupSize: 0,
-		numGroups: 0,
-		canSeeTail: 0,
-		appleTaxiMultiplier: 0,
-	};
-
-	// Assign the keys
-	for (let i = 0; i < objKeys.length; i++) {
-		config[objKeys[i]] = parseFloat(a[i]);
-	}
+	// Config is passed as a JSON string
+	const config = JSON.parse(argv[2]);
 
 	// Initialize the code in training mode and then quick-tick until the game
 	// ends.
